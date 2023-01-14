ad0a610db5f1dd2462d49b5b06156889754b1d40
SimonOesterberg <simonosterberg@hotmail.com>
2023-01-14 12:36:18 +0100

    Readability and improvements of Vision

--- Scripts/Vision.cs
+++ Scripts/Vision.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Vision : MonoBehaviour {
-    private VehicleController thisVehicle;
-    private float initialKph;
 
-    private List<Collider> collidersInSight = new List<Collider>();
-
+    // How far the vehicle shuld be able to see
     public int visionRange = 20;
 
+    // A list that will contain all colliders that the vehicle with this Vision script can see
+    private List<Collider> collidersInSight = new List<Collider>();
+
+    // A list that will contain all rays that will be sent out when looking
     private List<Ray> visionRays = new List<Ray>();
 
+    // Function that returns true if the passes collider: target can be seen
     public bool isSeeing(Collider target) {
+
+        // Send rays out and update collidersInSight
         lookAround();
 
+        // If the target collider can be seen return true
         foreach (Collider collider in collidersInSight) {
 
             if (collider == target) {
@@ -22,27 +27,33 @@ public class Vision : MonoBehaviour {
             }
         }
 
+        // If the target collider cannot be seen return false
         return false;
     }
 
+    // Function to send out the rays and update the collidersInSight list
     void lookAround() {
-        collidersInSight.Clear();
 
+        // Clear the collidersInSight list and visionRays list
+        collidersInSight.Clear();
         visionRays.Clear();
 
-        visionRays.Add(new Ray(transform.position, Quaternion.Euler(0, 5.0f, 0) * transform.forward));
-        visionRays.Add(new Ray(transform.position, transform.forward));
-        visionRays.Add(new Ray(transform.position, Quaternion.Euler(0, -5.0f, 0) * transform.forward));
+        // Add all rays that should be sent out by this vehicle to the visionRays list
+        /* Forward Left: */  visionRays.Add(new Ray(transform.position, Quaternion.Euler(0, 5.0f, 0) * transform.forward));
+        /* Forward: */       visionRays.Add(new Ray(transform.position, transform.forward));
+        /* Forward Right: */ visionRays.Add(new Ray(transform.position, Quaternion.Euler(0, -5.0f, 0) * transform.forward));
 
-        visionRays.Add(new Ray(transform.position, -transform.right));
-        visionRays.Add(new Ray(transform.position, transform.right));
+        /* Left: */          visionRays.Add(new Ray(transform.position, -transform.right));
+        /* Right: */         visionRays.Add(new Ray(transform.position, transform.right));
         
-        visionRays.Add(new Ray(transform.position, Quaternion.Euler(0, 5.0f, 0) * -transform.forward));
-        visionRays.Add(new Ray(transform.position, -transform.forward));
-        visionRays.Add(new Ray(transform.position, Quaternion.Euler(0, -5.0f, 0) * -transform.forward));
+        /* Back Left: */     visionRays.Add(new Ray(transform.position, Quaternion.Euler(0, 5.0f, 0) * -transform.forward));
+        /* Back: */          visionRays.Add(new Ray(transform.position, -transform.forward));
+        /* Back Right: */    visionRays.Add(new Ray(transform.position, Quaternion.Euler(0, -5.0f, 0) * -transform.forward));
 
+        // Stores the eventual hit of a ray
         RaycastHit hit;
 
+        // If a ray hits a collider add that collider to the collidersInSight list
         foreach (Ray visionRay in visionRays) {
             if (Physics.Raycast(visionRay, out hit, visionRange)) {    
                 collidersInSight.Add(hit.collider);
