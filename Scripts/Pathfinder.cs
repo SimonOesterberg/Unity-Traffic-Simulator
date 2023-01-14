2961dd0c87abdd5ea2d3486b460ea542e833873a
SimonOesterberg <simonosterberg@hotmail.com>
2023-01-14 12:10:07 +0100

    Readability and improvements of Pathfinder

--- Scripts/Pathfinder.cs
+++ Scripts/Pathfinder.cs
@@ -4,101 +4,118 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour {
 
-    public class LocalNode {
+    // Class used to store the nodes and their g, h and f values and which node led to that node
+    private class PathNode {
         public float g = 0;
         public float h = 0;
         public float f = 0;
 
-        public LocalNode parent;
-        public LaneNode node;
+        public PathNode parent;
+        public LaneNode laneNode;
 
-        public LocalNode(LocalNode parent, LaneNode node) {
+        public PathNode(PathNode parent, LaneNode laneNode) {
             this.parent = parent;
-            this.node = node;
+            this.laneNode = laneNode;
         }
     }
 
+    // Function that returns the names of each lane node that should be travelled to reach a vehicles end node
     public static List<string> getPath(LaneNode startNode, LaneNode endNode) {
 
+        // The list that should be returned by the function, starts empty
         List<string> path = new List<string>();
 
-        LocalNode localStartNode = new LocalNode(null, startNode);
-        LocalNode localEndNode   = new LocalNode(null, endNode  );
-        LocalNode currentLocalNode;
+        // Create a path node of the start and end lane node
+        PathNode pathStartNode = new PathNode(null, startNode);
+        PathNode pathEndNode   = new PathNode(null, endNode  );
+        PathNode currentPathNode;
 
-        List<LocalNode> nodesToVisit = new List<LocalNode>();
-        nodesToVisit.Add(localStartNode);
+        // Lists to store nodes still to visit and nodes that have been visited
+        List<PathNode> pathNodesToVisit = new List<PathNode>();
+        List<PathNode> visitedPathNodes = new List<PathNode>();
 
-        List<LocalNode> visitedNodes = new List<LocalNode>();
+        // Add the start node of the path to the list of path nodes to visit
+        pathNodesToVisit.Add(pathStartNode);
 
-        int loops = 0;
+        while (pathNodesToVisit.Count > 0) {
+            // While there are still nodes to visit and the end node has not been found:
 
-        while (nodesToVisit.Count > 0 && loops < 200) {
+            // Set the current path node to the first node in the list if paths to visit
+            currentPathNode = pathNodesToVisit[0];
 
-            loops++;
-
-            currentLocalNode = nodesToVisit[0];
-
-            for (int i = 0; i < nodesToVisit.Count; i++) {
-                if (nodesToVisit[i].f < currentLocalNode.f) {
-                    currentLocalNode = nodesToVisit[i];
+            // If any of the other nodes in the list of nodes to visit have a better f value, set that to the current path node
+            for (int i = 1; i < pathNodesToVisit.Count; i++) {
+                if (pathNodesToVisit[i].f < currentPathNode.f) {
+                    currentPathNode = pathNodesToVisit[i];
                 }
             }
 
-            visitedNodes.Add(currentLocalNode);
-            nodesToVisit.Remove(currentLocalNode);
+            // Add the current path nodes to the visited list and remove it from the list of path nodes to visit
+            visitedPathNodes.Add(currentPathNode);
+            pathNodesToVisit.Remove(currentPathNode);
 
-            if (currentLocalNode.node.gameObject.name == localEndNode.node.gameObject.name) {
-                
-                LocalNode current = currentLocalNode;
+            if (currentPathNode.laneNode.gameObject.name == pathEndNode.laneNode.gameObject.name) {
+                // If the current path node has the same name as the end node:
 
-                while (current != null) {
-                    path.Add(current.node.gameObject.name);
-                    current = current.parent;
+                // Add all path nodes on the way to the end node to the path list
+                while (currentPathNode != null) {
+                    path.Add(currentPathNode.laneNode.gameObject.name);
+                    currentPathNode = currentPathNode.parent;
                 }
 
+                // Reverse and return it
                 path.Reverse();
-
                 return path;
             }
 
-            List<LocalNode> neighbours = new List<LocalNode>();
+            // List of connected path nodes to the current path node
+            List<PathNode> connectedPathNodes = new List<PathNode>();
 
-            foreach (LaneNode connectedNode in currentLocalNode.node.connectedLaneNodes) {
-                neighbours.Add(new LocalNode(currentLocalNode, connectedNode));
+            // Add all of the connected nodes to the connected path nodes list
+            foreach (LaneNode connectedNode in currentPathNode.node.connectedLaneNodes) {
+                connectedPathNodes.Add(new PathNode(currentPathNode, connectedNode));
             }
 
-            
+            foreach (PathNode connectedPathNode in connectedPathNodes) {
+                // For every connected path node:
 
-            foreach (LocalNode neighbour in neighbours) {
-                
+                // Boolean to store wether a path node has already been visited before or not
                 bool hasBeenVisited = false;
 
-                foreach (LocalNode visitedNeighbour in visitedNodes) {
-                    if (neighbour == visitedNeighbour) {
+                // If the connected node is in the list of visited path nodes set hasBeenVisited to true
+                foreach (PathNode visitedPathNode in visitedPathNodes) {
+                    if (connectedPathNode == visitedPathNode) {
                         hasBeenVisited = true;
                         break;
                     }
                 }
 
                 if (!hasBeenVisited) {
-                    Vector3 p0 = neighbour.node.transform.position;
-                    Vector3 p1 = localEndNode.node.transform.position;
+                    // If the connected path node has not been visited before:
 
-                    neighbour.g = currentLocalNode.g + 1;
-                    neighbour.h = Mathf.Pow((Mathf.Pow(p1.x-p0.x, 2) + Mathf.Pow(p1.y-p0.y, 2) + Mathf.Pow(p1.z-p0.z, 2)), 0.5f);
-                    neighbour.f = neighbour.g + neighbour.h;
+                    // Store the position of the current path nodes laneNode and end laneNode as p0 and p1
+                    Vector3 p0 = connectedPathNode.laneNode.transform.position;
+                    Vector3 p1 = endNode.transform.position;
 
-                    foreach (LocalNode open_node in nodesToVisit) {
-                        if (neighbour == open_node && neighbour.g > open_node.g) {
-                            nodesToVisit.Remove(open_node);
+                    // Set the g value to the steps from the start node, h to the distance to the end node and f to the sum of g and h
+                    connectedPathNode.g = currentPathNode.g + 1;
+                    connectedPathNode.h = Vector3.Distance(p0, p1);
+                    connectedPathNode.f = connectedPathNode.g + connectedPathNode.h;
+
+                    // If the connected path node is in the list of path nodes to visit and this is a closer way to it, remove the other way to it
+                    foreach (PathNode pathNodeToVisit in pathNodesToVisit) {
+                        if (connectedPathNode == pathNodeToVisit && connectedPathNode.g > pathNodeToVisit.g) {
+                            pathNodesToVisit.Remove(pathNodeToVisit);
                         }
                     }
 
-                    nodesToVisit.Add(neighbour);
+                    // Add the connected path node to the list of path nodes left to visit
+                    pathNodesToVisit.Add(connectedPathNode);
                 }
             }
         }
+
+        // If the end was not found, return the empty path
         return path;
     }
 }
